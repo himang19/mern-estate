385d00099e9172617fff23386b11911823697269
himang19 <himanshu19october@gmail.com>
2024-04-28 19:24:18 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import SingIn from './pages/SingIn';
+import SiunUp from './pages/SiunUp';
+import About from './pages/About';
+import Profile from './pages/profile';
+import { jsxDEV as _jsxDEV } from "react/jsx-dev-runtime";
 export default function App() {
-  return (
-    <h1 className='text-red-500'>Bipul Kumar Dikshit and Himanshu Agarwal</h1>
-  )
+  return  <BrowserRouter>
+         <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/sign-in' element={<SingIn/>} />
+      <Route path='/sign-up' element={<SiunUp />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/profile' element={<Profile />} />
+
+      </Routes>
+      </BrowserRouter>
 }
